8a7e04de876e6942b1f42225ebe9b400da15336a
Cristian <krishenaosanta@hotmail.com>
2024-03-17 20:06:34 -0500

    portafolio page

---
+++ src/components/core/PrFlipCard.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front" :style="{backgroundImage: `url(${prod.picture})`}">
+                <div class="flip-card_protrait">
+                    <h3>{{ prod.title }}</h3>
+                    <div class="flip-tags">
+                        <span v-for="tag in prod.tags" class="flip-tag">{{ tag }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-card-back">
+                <h3>{{ prod.title }}</h3>
+                <p class="short-description">{{ prod.description }}</p>
+                <a :href="prod.link" target="_blank" class="flip-cta">see more</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+
+const {prod} = defineProps(["prod"]);
+
+</script>
+
+<style>
+.flip-card {
+  background-color: transparent;
+  width: 250px;
+  height: 300px;
+  perspective: 1000px;
+  border-radius: 3px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+  border-radius: 10px;
+  padding: 10px;
+}
+
+.flip-card-front {
+  background-color: #bbb;
+  color: white;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.flip-card-back {
+  background-color: blueviolet;
+  color: white;
+  transform: rotateY(180deg);
+  display: flex;
+  flex-direction: column;
+}
+
+.flip-card-back .short-description {
+    flex: 5;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: left;
+    max-height: calc(1rem * 33);
+
+}
+
+.flip-card_protrait {
+    background-color: #000a;
+    border-radius: 10px;
+    height: calc(100% + 20px);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    margin: -10px;
+}
+.flip-tag {
+    display: inline-block;
+    margin: 2px;
+    padding: 5px;
+    background-color: gray;
+    border-radius: 10px;
+}
+
+.flip-tags {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.flip-cta{
+    background-color: white;
+    border-radius: 10px;
+    margin: 5px;
+    text-decoration: none;
+    color: blueviolet;
+}
+</style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,29 +1,106 @@
 <template>
     <pr-page-template title="Portfolio" logo="portfolio">
-      <pr-card>
-        <h3>Profesionals</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod voluptates sit tempore beatae itaque explicabo! Maxime perspiciatis odit soluta esse neque sint animi! Totam minima ad nihil error, tempora nostrum?
-        </p>
-      </pr-card>
-      <pr-card>
-        <h3>Personal</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi distinctio, quis exercitationem optio corporis nesciunt rem, omnis officia quas ut dignissimos ratione, mollitia similique sequi sed tempore neque placeat consectetur?
-        </p>
-      </pr-card>
+      <section class="pt-grid">
+        <pr-flip-card v-for="prod in prods" :prod="prod"/>
+      </section>
     </pr-page-template>
   </template>
   
   <script setup>
   
   import PrPageTemplate from '@/components/core/PrPageTemplate.vue';
-  import PrCard from '@/components/core/PrCard.vue';
+  import PrFlipCard from '@/components/core/PrFlipCard.vue';
   
+  const prods = [
+    {
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"Uber",
+      picture:"https://pbs.twimg.com/profile_images/1042867341476995078/antCC8gJ_400x400.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam rem natus debitis placeat, vitae possimus? Architecto rerum ipsum, ad quasi nemo atque ab quo nam necessitatibus, quod minus repellat porro. Lorem impsu dolor sit amep",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    },{
+      title:"discovery app",
+      picture:"https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/82/58/59/8258596e-9969-c19e-d0b1-ee26abd8bd4a/source/512x512bb.jpg",
+      tags: ["niños", "entretenimiento", "applicación"],
+      description: "lorem impsum",
+      link: "/portafolio/1"
+    }
+  ]
   </script>
   
   <style scoped>
     h3 {
       font-size: 32px;
+    }
+    .pt-grid {
+      display: grid;
+      grid-template-columns: auto auto auto;
+      grid-gap: 40px; 
     }
   </style>
